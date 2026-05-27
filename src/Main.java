5a847c19323db7ba7eb891f5e9f6cd648507ee50
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-05-27 15:05:19 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,21 @@ class PrimeiraClasse {
         System.out.println("Ano de Lançamento " + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 9.7) /3;
+        System.out.println(media);
+        String sinopseDoFilme;
+        sinopseDoFilme = """
+                Sobre o filme:
+                Filme Top Gun
+                Filme de Aventura
+                Super recomendo
+                Ano de Lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopseDoFilme);
+
+
+
     }
 
 }
