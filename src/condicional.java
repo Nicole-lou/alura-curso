bb9e05ef629e261496ea633cc76f5e3259d555a0
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-06-04 18:35:31 -0300

    Create condicional.java

---
+++ src/condicional.java
@@ -0,0 +1,23 @@
+public class condicional {
+    static void main() {
+        int anoDeLancamento = 1990;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+
+        if(anoDeLancamento >= 2022){
+            System.out.println("Lançamento que os clientes estão curtindo!");
+
+        } else {
+            System.out.println("Filme retrô que vale a pena de assistir!");
+        }
+
+        if(incluidoNoPlano == true || tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Filme não liberado, não tem o plano plus");
+        }
+    }
+
+}
