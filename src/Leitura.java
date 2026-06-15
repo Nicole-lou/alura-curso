4dc57f8dbbca616a4675512ea85bee2be2942ad8
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-06-15 14:24:59 -0300

    Update Leitura.java

--- src/Leitura.java
+++ src/Leitura.java
@@ -4,5 +4,19 @@ public class Leitura {
     static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
 
+        System.out.println("Digite seu filme favorito: ");
+        String filme = leitura.nextLine();
+
+        System.out.println("Qual ano de lançamento? ");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Sua avaliação para o filme: ");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+
+
+
     }
 }
